ce8ef8fbc53015c8a4d90afc90d59d5d55a054ba
Тимофей Мещеряков <tdmesherykov@mail.ru>
2026-04-13 17:11:31 +0300

    Step 10: dynamic DOM element creation

--- main.js
+++ main.js
@@ -23,3 +23,57 @@ btn.addEventListener("click", () => {
     btn.style.backgroundColor = "#4CAF50";
     btn.style.color = "white";
 });
+
+const input = document.getElementById("nameInput");
+const button = document.getElementById("showName");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+if (input.ariaValueMax.trim() !== "") {
+    output.textContent = `Привет, ${input.value}!`;
+    output.style.color = "green";
+} else {
+    output.textContent = "Пожалуйста, введите имя!";
+    output.style.color = "red";
+}
+});
+
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", () =>{
+    input.value = "";
+    output.textContent = "";
+    output.style.color = "";
+    input.focus();
+});
+
+const from = document.getElementById("userForm");
+const usernameInput = document.getElementById("username");
+const ageInput = document.getElementById("age");
+const result = document.getElementById("formResult");
+
+from.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const name = usernameInput.value.trim();
+    const age = Number(ageInput.value);
+    if (name === "") {
+        result.textContent = "Ошибка: Имя не может быть пустым";
+        result.style.color = "red";
+        usernameInput.focus();
+        return;
+    }
+
+    if (isNaN(age) || age<=0|| age> 120){
+        result.textContent = "Ошибка: Введите корректно возраст";
+        result.style.color = "red";
+        ageInput.focus();
+        ageInput.value =  "";
+        return;
+    }
+    result.textContent = `Данные сохранены:${name},${age}`;
+    result.style.color = "green";
+
+    from.reset();
+});
+
+
+
